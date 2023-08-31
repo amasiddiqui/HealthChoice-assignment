c5aa8c08df873416cfc3a6f037a5eba86992c6a4
amasiddiqui <amasiddiqui@deloitte.com>
2023-08-31 22:25:44 +0530

    commit

--- healthchoice-assignment/src/App.js
+++ healthchoice-assignment/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
+import HomePage from "./components/homePage";
 import Navbar from "./components/Navbar";
 
 function App() {
